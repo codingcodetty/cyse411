aaf8d39b7369495fc71a06429fde6d621937f2ad
codingcodetty <248531299+codingcodetty@users.noreply.github.com>
2025-12-09 21:49:18 -0500

    Update server.js

--- fastbank_insecure_classwork/backend/server.js
+++ fastbank_insecure_classwork/backend/server.js
@@ -8,6 +8,16 @@ const crypto = require("crypto");
 
 const app = express();
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // max 100 requests per windowMs
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 // --- BASIC CORS (clean, not vulnerable) ---
 app.use(
   cors({
